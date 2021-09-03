92acfe3ceda2fd0fdd7b26bf88c9d0f991d46296
Pedro Gomes <pgsantanadesouza@usp.br>
2021-09-03 19:46:15 -0300

    Deploy na heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,12 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    user: "postgres",
-    host: "localhost",
-    database: "market_cubos",
-    password: "1234",
+    user: "ieipqykdorldzs",
+    host: "ec2-54-242-43-231.compute-1.amazonaws.com",
+    database: "d4t5ukjudg5q91",
+    password:
+      "667069e0385f1b6cef3f5eec0767fade24659fc623e68374bc3f130c5736bc7e",
+    port: 5432,
   },
 });
 
